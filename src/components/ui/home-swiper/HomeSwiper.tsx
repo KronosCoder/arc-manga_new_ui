2a6f01a5663848d69ea7f71e2a545ce3f106d0d5
KronosCoder <atasit.saengsak@gmail.com>
2025-08-22 10:03:50 +0700

    implement hero section optimize seo

--- src/components/ui/home-swiper/HomeSwiper.tsx
+++ src/components/ui/home-swiper/HomeSwiper.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import React, { RefObject, useState } from 'react';
+import React, { RefObject, useCallback, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules';
 import Image from 'next/image';
+import './HomeSwiper.css';
 
 import 'swiper/css';
 
@@ -16,6 +17,14 @@ export default function HomeSwiper({ prevButton, nextButton }: HomeSwiperProps) 
     const mock_genres = ["manhwa", "action", "fantasy", "adventure"];
     const [isRecommended, setIsRecommended] = useState<boolean>(true);
 
+    const handleBeforeInit = useCallback((swiper: any) => {
+        const navigation = swiper.params.navigation;
+        if (navigation && typeof navigation !== 'boolean') {
+            navigation.prevEl = prevButton?.current ?? undefined;
+            navigation.nextEl = nextButton?.current ?? undefined;
+        } 
+    }, [nextButton, prevButton])
+
     return (
         <Swiper
             className='w-full h-[325px] lg:h-[425px]'
@@ -23,28 +32,39 @@ export default function HomeSwiper({ prevButton, nextButton }: HomeSwiperProps) 
             modules={[Navigation]}
             loop={true}
             slidesPerView={1}
-            onBeforeInit={(swiper) => {
-                const navigation = swiper.params.navigation;
-                if (navigation && typeof navigation !== 'boolean') {
-                navigation.prevEl = prevButton?.current ?? undefined;
-                navigation.nextEl = nextButton?.current ?? undefined;
-                } 
-            }}
+            onBeforeInit={handleBeforeInit}
             onSlideChange={() => console.log('slide change')}
             onSwiper={(swiper) => console.log(swiper)}  
         >
             <SwiperSlide className='w-full h-full relative'>
                 <div className="group cursor-pointer h-full">
-                    <section className="banner__section relative overflow-hidden shadow-lg z-[1] h-full">
+                    <section 
+                        className="banner__section relative overflow-hidden shadow-lg z-[1] h-full"
+                        aria-label="Featured Content Slider"
+                        role="region"
+                    >
                         <div className="absolute top-16 lg:top-24 w-full h-full flex flex-col z-[101] py-2">
                             <div className="page__container">
                                 <div className="w-full flex gap-3">
+                                    {/* Recommended Content Image */}
                                     <div className="rounded-md shadow-md  min-w-28 lg:min-w-52 h-[10rem] lg:h-[18rem] aspect-[7/10] overflow-hidden">   
-                                        <img src="/mock/mock_2.jpg" className='w-full h-full object-cover transition-all duration-200 ease-in-out hover:scale-125 hover:brightness-105' alt="" />
+                                        <img src="/mock/mock_2.jpg" className='w-full h-full object-cover cursor-pointer' alt="" />
                                     </div>
-                                    <div className="flex flex-col flex-grow flex-wrap">
-                                        <h1 className='font-bold text-xl lg:text-4xl'>Solo Leveling ( All Seasons )</h1>
-                                        <div className="hidden lg:flex flex-wrap gap-1 my-3">   
+                                    {/* Recommended Content Details */}
+                                    <div className="relative flex flex-col flex-grow flex-wrap">
+                                        {/* Title */}
+                                        <header>
+                                            <h1 className='font-bold text-2xl lg:text-4xl'>Solo Leveling ( All Seasons )</h1>
+                                        </header>
+                                        {/* Genres */}
+                                        <div className="hidden sm:flex flex-wrap gap-1 my-3">   
+                                            {isRecommended && (
+                                                <span 
+                                                    className="genres inline-flex items-center rounded uppercase text-[0.625rem] font-bold px-[0.375rem] leading-[1.5em] my-auto text-white bg-orange-400"
+                                                >
+                                                    Recommended
+                                                </span>
+                                            )}
                                             {mock_genres.map((genre) => (
                                                 <span 
                                                     className="genres inline-flex items-center rounded uppercase text-[0.625rem] font-bold px-[0.375rem] leading-[1.5em] my-auto text-gray-900 bg-gray-50"
@@ -54,13 +74,22 @@ export default function HomeSwiper({ prevButton, nextButton }: HomeSwiperProps) 
                                                 </span>
                                             ))}
                                         </div>
+                                        {/* Descrioption */}
+                                        <article className="hide-scrollbar hidden lg:block w-[90%] h-full max-h-[150px] overflow-y-scroll flex-wrap">
+                                            <p className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, omnis quaerat perferendis officia distinctio impedit, possimus reiciendis iure tempore natus exercitationem beatae, quisquam doloribus corporis! Quaerat et odit itaque earum!</p>
+                                        </article>
+
+                                        <footer className="absolute bottom-0 left-0 w-full">
+                                            <p className='text-sm font-medium'>John Doe</p>
+                                        </footer>
                                     </div>
                                 </div>
                             </div>
                         </div>
+                        {/* Genres for Mobile */}
                         <div className="absolute w-full bottom-10 left-0 z-10">
                             <div className="page__container">
-                                <div className="lg:hidden flex items-center justify-start flex-wrap gap-1 my-3">   
+                                <div className="sm:hidden flex items-center justify-start flex-wrap gap-1 my-3">   
                                     {isRecommended && (
                                         <span 
                                             className="genres inline-flex items-center rounded uppercase text-[0.625rem] font-bold px-[0.375rem] leading-[1.5em] my-auto text-white bg-orange-400"
